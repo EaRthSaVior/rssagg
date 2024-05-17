3ee1cc12c65bf6a3c1975aadc1b22528c20575dc
Teoh Soon Ann <101308101+EaRthSaVior@users.noreply.github.com>
2024-05-18 02:01:07 +0800

    add APIs

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/EaRthSaVior/rssagg/internal/database"
 	"github.com/go-chi/chi"
@@ -35,10 +36,13 @@ func main() {
 		log.Fatal("Can't connect to database")
 	}
 
+	db := database.New(conn)
 	apiCfg := apiConfig{
-		DB:database.New(conn),
+		DB: db,
 	}
 
+	go startScaping(db, 10, time.Minute)
+	
 	router:= chi.NewRouter()
 
 	router.Use(cors.Handler(cors.Options{
@@ -53,7 +57,20 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz",handlerReadiness)
 	v1Router.Get("/error", handlerErr)
+
+	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerGetUser))
 	v1Router.Post("/users", apiCfg.handlerCreateUser)
+
+	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
+	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
+
+	v1Router.Get("/posts", apiCfg.middlewareAuth(apiCfg.handlerGetPostsForUser))
+
+	v1Router.Post("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerCreateFeedFollow))
+	v1Router.Get("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerGetFeedFollow))
+	v1Router.Delete("/feed_follows/{feedFollowID}", apiCfg.middlewareAuth(apiCfg.handlerDeleteFeedFollow))
+
+	
 	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
